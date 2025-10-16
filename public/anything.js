7c5b0555e1ff64714874a812267fb90aafbb5bb9
jadcperez <b2029jadcperez@pshs.edu.ph>
2025-10-16 15:53:15 +0800

    Second Commit

---
+++ public/anything.js
@@ -0,0 +1,37 @@
+console.log('Hey there Sir Roy! :)')
+
+//gma - 7
+//abs-cbn - 2
+//tv5 - 5
+//ibc - 13
+//ptv - 4
+
+rl = require('readline')
+rli = rl.createInterface({
+    input: process.stdin,
+})
+rli.on('line', give_station)
+
+function give_station(num) {
+    switch (Number(num)) {
+        case 7:
+            console.log('gma' + ' - 7')
+            break
+        case 2:
+            console.log('abs-cbn' , '- 2')
+            break
+        case 5:
+            first_letter = "t"
+            console.log(`${first_letter}v5 - 5`)
+            break
+        case 13:
+            console.log('ibc - 13')
+            break
+        case 4:
+            console.log('ptv - 4')
+            break
+        default:
+            console.log('Unknown station')
+            break
+    }   
+}
--- public/hi.md
+++
@@ -1,8 +0,0 @@
-{
-    Name: "Jose Angelo Perez",
-    Grade: "9",
-    Category: "traditional",
-    Description: "I am wearing a Barong Tagalog borrowed from my younger brother... Somehow it isn't too small and it fits",
-    Image: "images/samples/student1.jpg",
-    Date: "2025-08-04"
-}
